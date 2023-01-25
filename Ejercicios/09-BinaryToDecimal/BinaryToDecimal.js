f008bb21b5043cb3846ea597df783303bf0a81f6
AlexT-Dev <atacolmx@gmail.com>
2023-01-25 16:28:26 -0600

    Binario a decimal y viceversa

--- Ejercicios/09-BinaryToDecimal/BinaryToDecimal.js
+++ Ejercicios/09-BinaryToDecimal/BinaryToDecimal.js
@@ -1,6 +1,17 @@
-function BinaryToDecimal (binary) {
+function BinaryToDecimal (decimal) {
   // Your code here:
-
+  let decimalValue = 0
+  
+  let cadena = decimal.split("");
+ for (let i = 0; i < cadena.length; i++) {
+    
+    decimalValue = decimalValue + cadena[i] * Math.pow(2,parseInt((cadena.length-i)-1,10));
+ }
+ 
+  console.log(decimalValue);
+  return decimalValue
 }
 
 module.exports = BinaryToDecimal
+
+console.log(BinaryToDecimal("1101100"))
--- Ejercicios/10-DecimalToBynary/DecimalToBynary.js
+++ Ejercicios/10-DecimalToBynary/DecimalToBynary.js
@@ -1,6 +1,18 @@
 function DecimalToBynary (num) {
   // Your code here:
-
+  
+  let number = num;
+  let binary = (number % 2).toString();
+  for (; number > 1; ) {
+      number = parseInt(number / 2);
+      binary =  (number % 2) + (binary);
+  }
+  console.log(binary);
+  return binary
+  
 }
 
 module.exports = DecimalToBynary
+
+
+console.log(DecimalToBynary(12))
