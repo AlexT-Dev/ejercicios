f6ef0abe97ada78f8a086a60392c45646a85c704
AlexT-Dev <atacolmx@gmail.com>
2023-01-24 20:54:44 -0600

    Cambios

--- Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
+++ Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
@@ -1,6 +1,24 @@
 function FindWordStartingWith (book, query) {
   // Your code here:
-
+  let result = [];
+  text = book.text.toLowerCase();
+  for (let i = 0; i < text.length; i++) {
+    if (text[i -1] === " " || i ===0 ){
+      for (let j = 0; j < query.length; j++) {
+        if (query[j] === text[i + j]){
+          if (j === query.length -1 ) result.push(i);
+        } else break;
+        
+      }
+    }
+  }
+  return result
 }
 
 module.exports = FindWordStartingWith
+
+const book = {
+  id: 1,
+  text: 'Erase una vez un libro de palabras que era un poco aburrido pero tenia mucho aunque algunas palabras locas'
+}
+console.log(FindWordStartingWith (book, 'de'))
