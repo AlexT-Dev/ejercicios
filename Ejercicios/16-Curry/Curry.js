2ec3c47afa584b6d5b7f5a648afc13e5f4bbe0b4
AlexT-Dev <atacolmx@gmail.com>
2023-01-26 17:52:28 -0600

    Curry termindo

--- Ejercicios/16-Curry/Curry.js
+++ Ejercicios/16-Curry/Curry.js
@@ -1,6 +1,24 @@
 function curry (fn) {
   // Your code here:
+   const args = [];
+   const argsNum = fn.length;
 
+
+   return function curried (arg){
+      return args.push(arg) === argsNum ? fn(...args) : curried;
+   };
 }
 
 module.exports = curry
+
+
+const sumar = (a,b,c,d) => a + b - c * d;
+
+const curriedDoSomething = curry(sumar);
+const firstReturn = curriedDoSomething(1);
+const secondReturn = firstReturn(2);
+const thirdReturn = secondReturn(3);
+const fourthReturn = thirdReturn(10);
+
+console.log(fourthReturn);
+
